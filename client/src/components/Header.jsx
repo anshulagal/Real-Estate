40335807f38339efd88c0a4407e09fd1f0b2f35b
SparshDusad <121246173+SparshDusad@users.noreply.github.com>
2023-10-18 20:58:36 +0530

    Update Header.jsx
    Added logo visibility in the window

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,6 +2,7 @@ import { FaSearch } from 'react-icons/fa';
 import { Link, useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { useEffect, useState } from 'react';
+import Logo from '/Logo.jpeg';
 
 export default function Header() {
   const { currentUser } = useSelector((state) => state.user);
